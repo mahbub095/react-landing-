8b9c0ea36f3ca98103e330272fb109815292238e
mahbub095 <mahbub.095@gmail.com>
2024-09-19 15:43:10 +0600

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet,Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hi Developeres</h1>
+      <nav>
+        <ul className='flex space-x-8 mb-8 bottom-0'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet></Outlet>
+      <footer>Footer</footer>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path: "/",
+        element: <h1>Home main</h1>
+      },
+      {
+        path: "/about",
+        element: <h1>About main</h1>
+      },
+      {
+        path: "/contact",
+        element: <h1>Contact main</h1>
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
+ 
