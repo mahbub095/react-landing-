e392156f7ef73a1057a35443883472e455de6cee
mahbub095 <mahbub.095@gmail.com>
2024-09-20 15:43:36 +0600

    NavItems Show

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// Define an array of nav items
+const navItems = [
+    { path: '/', label: 'Furniture' },
+    { path: '/shop', label: 'Shop' },
+    { path: '/about', label: 'About Us' },
+    { path: '/contact', label: 'Contact' },
+];
+
+const NavItems = () => {
+    return(
+        <ul>
+            {
+                navItems.map((item,index)=> (
+                    <li>
+                        <Link to ={item.path}>{item.label}</Link>
+
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
 
 const Navbar = () => {
     return (
         <header>
-            <nav className='container mx-auto flex justify-between items-center py-6 px-4'>
+            <nav className='max-w-screen-2xl container mx-auto flex justify-between items-center py-6 px-4'>
                 {/* logo */}
-                <a href="/">Logo</a>
+                <Link to="/" className="font-bold">Logo</Link>
                 {/* desktop menu items */}
-                <div> Menu items</div>
+                <div>
+                    <NavItems/>
+                </div>
+               
                 {/* Cart icon */}
                 <div> cart</div>
             </nav>
